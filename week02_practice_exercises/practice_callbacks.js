cfd45a3524a6c4dc9dbfca7e97e37f5e561b2148
Justin Samuels <blizm87@gmail.com>
2016-12-09 00:30:57 -0800

    Completed practice_callbacks.js

--- week02_practice_exercises/practice_callbacks.js
+++ week02_practice_exercises/practice_callbacks.js
@@ -1,28 +1,39 @@
 var randomName = function() {
-       var name = ['George', 'Mike', 'Joe', 'Natalie'];
-       //RETURN A RANDOM NAME FROM THE name ARRAY
-       //YOU CAN USE Math.random method
+      var name = ['George', 'Mike', 'Joe', 'Natalie'];
+      var count = Math.random()*4;
+      if (count < 1 && count >0){
+          return name[0];
+      } else if (count >= 1 && count < 2){
+          return name[1];
+        } else if (count >= 2 && count < 3){
+            return name[2];
+          } else {
+              return name[3];
+            }
+
    }
 
     var say = function(saysomething) {
-      console.log('Say ', x);
+      console.log('Say ' + saysomething);
     }
     var yell = function(yellsomething) {
-      console.log('Yell ', x);
+      console.log('Yell ' + yellsomething);
     }
     var eat = function(eatsomething) {
-      console.log('Eat ', x);
+      console.log('Eat ' + eatsomething);
     }
 
     var roll = function(rollsomething) {
-      console.log ('Roll ', x);
+      console.log ('Roll ' + rollsomething);
     }
 
     function random_function(a, x, y, z, f) {     //the parameters can be anything, as long as they are used within the function
       //x,y,z,f parameters are FUNCTIONS, call these functions using a as parameter. LOOK AT EXAMPLE BELOW
+      console.log(a);
     }
+    random_function('Possible results include:' ,say(randomName()),yell(randomName()),eat(randomName()),roll(randomName()));
 
-    random_function(randomName, say, yell, eat, roll);    //When you call the function make sure to use the correct function name. IMP!
+    //random_function(randomName, say, yell, eat, roll);    //When you call the function make sure to use the correct function name. IMP!
     // Possible results include:
     // Roll George
     // Eat Ninja
